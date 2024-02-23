a210c44679856ffbee8b97ca0d56e8e9dd421367
abhiram945 <abhiram94567@gmail.com>
2024-02-23 18:40:34 +0530

    added shuffle

--- src/index.jsx
+++ src/index.jsx
@@ -7,32 +7,43 @@ import Downloadsong from './downloadSong';
 import { createContext, useState, useEffect } from 'react';
 
 const Audic=()=>{
-  const [apiData, setApiData] = useState(null);
-  const [currentSong, setCurrentSong]=useState(null);
-  const [songIndex, setSongIndex]=useState(0);
-  const [listIndex, setListIndex]=useState(0);
+    const [apiData, setApiData] = useState(null);
+    const [currentSong, setCurrentSong]=useState(null);
+    const [songIndex, setSongIndex]=useState(0);
+    const [listIndex, setListIndex]=useState(0);
+    const shuffleData = (data) => {
+    const shuffledData = { ...data };
+    shuffledData.allLists = shuffledData.allLists.map(array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    });
+    return shuffledData;
+    };
     useEffect(() => {
-        const fetchData = async () => {
-        try{
-            await fetch('assets/api.json').then(response=>response.json()).then(jsonData=>setApiData(jsonData));
-        }
-        catch(error){
-            console.error();
-        }
-        };
-        fetchData();
+    const fetchData = async () => {
+    try{
+        await fetch('assets/api.json').then(response=>response.json()).then(jsonData=>shuffleData(jsonData)).then(shuffledData=>setApiData(shuffledData));
+    }
+    catch(error){
+        console.error();
+    }
+    };
+    fetchData();
     }, []);
     return <>
         <musicContext.Provider value={{apiData, setApiData, currentSong, setCurrentSong, songIndex, setSongIndex, listIndex, setListIndex}}>
-          <BrowserRouter>
+            <BrowserRouter>
             <Header/>
             <Routes>
-              <Route path='/audic' element={<Main/>}/>
-              <Route path='/audic/:song' element={<Downloadsong/>}/>
+                <Route path='/audic' element={<Main/>}/>
+                <Route path='/audic/:song' element={<Downloadsong/>}/>
             </Routes>
             <Footer/>
-          </BrowserRouter>
-      </musicContext.Provider>
+            </BrowserRouter>
+        </musicContext.Provider>
     </>
 }
 
